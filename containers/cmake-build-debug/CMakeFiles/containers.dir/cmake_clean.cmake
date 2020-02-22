965114652a382baa8ab35a1671f3a944ee89986f
Ishaan Sehgal <ishaanforthewin@gmail.com>
2020-02-22 01:07:50 -0600

    Passes all local test cases do not understand why it is wrong

---
+++ containers/cmake-build-debug/CMakeFiles/containers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/containers.dir/main.cpp.o"
+  "containers"
+  "containers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/containers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ containers/main.cpp
@@ -0,0 +1,60 @@
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <cstdio>
+#include <set>
+using namespace std;
+
+int main() {
+    char *input = nullptr;
+    size_t size;
+
+    int case_num = 1;
+    while(getline(&input, &size, stdin) >= 0 && strcmp(input, "end") != 0){
+        if(input[strlen(input) - 1] == '\n'){
+            input[strlen(input) - 1] = '\0';
+        }
+
+        vector<stack<char> > stacks;
+
+        for(size_t i = 0; i < strlen(input); i++){
+            if(stacks.empty()){
+                stack<char> tmp;
+                tmp.push(input[i]);
+                stacks.push_back(tmp);
+            } else {
+                size_t min_idx = -1;
+                size_t min_dist = 100;
+
+                for(size_t idx = 0; idx < stacks.size(); idx++){
+                    size_t dist = stacks[idx].top() - input[i];
+                    if(dist >= 0 && dist < min_dist){
+                        min_dist = dist;
+                        min_idx = idx;
+                    }
+                }
+
+                if(min_idx != -1){
+                    stacks[min_idx].push(input[i]);
+                } else {
+                    stack<char> tmp;
+                    tmp.push(input[i]);
+                    stacks.push_back(tmp);
+                }
+            }
+        }
+
+        set<char> unique_chars;
+        for(size_t x = 0; x < strlen(input); x++)
+            unique_chars.insert(input[x]);
+
+
+        int res = unique_chars.size() < stacks.size() ? unique_chars.size() : stacks.size();
+        printf("Case %d: %d\n", case_num, res);
+
+        case_num++;
+    }
+
+
+    return 0;
+}
